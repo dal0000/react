7abc241aec738d9dc6ec797f445a3cfdd1290048
dal10000 <dal10000@hanmail.net>
2023-07-03 14:59:22 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -4,35 +4,63 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   const [money, setMoney] = useState(0);
+  let itemPrice;
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers?quotes=krw")
-    .then((response)=>response.json())
-    .then((json)=>{
-      setCoins(json);
-      setLoading(false);
-    }); 
-  },[]);
-  const onChange = (event) => setMoney(event.target.value);
-  const onSubmit = () => {
-    setMoney(0);
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const moneyChange = (event) => {
+    setMoney(event.target.value);
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    itemPrice = event.target.querySelector("select").value;    
+    showAmount();
   };
-   return (
+  const coinChange = (event) => {
+    itemPrice = event.target.value;
+    showAmount();
+  };
+  const showAmount = () => {
+    console.log(`내가 살 수 있는 양은 ${(money / itemPrice).toFixed(3)}`); 
+  };
+  return (
     <div>
-      {loading? <h1>Loading...</h1>: 
-      <div>
-        <h1> Coins!! ({loading? "" : coins.length})</h1>
-        <form onSubmit={onSubmit}>
-          <div>
-            <label htmlFor="money"> 시드머니 : 
-              <input id="money" type="number" onChange={onChange} value={money}></input>
-            </label>
-          </div>
-          <select>
-          {coins.map(item => <option>{item.name}({item.symbol}) price : {Math.round(item.quotes.KRW.price)} 원</option>)}
-          </select>
-        </form>
-      </div>}
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1> Coins!! ({coins.length})</h1>
+          <form onSubmit={onSubmit}>
+            <select onChange={coinChange}>
+              {coins.map((item) => (
+                <option value={item.quotes.KRW.price} key={item.id}>
+                  {item.name}({item.symbol}) ￦ 
+                  {Math.round(item.quotes.KRW.price).toLocaleString()} 원
+                </option>
+              ))}
+            </select>
+            <div>
+              <label htmlFor="money">
+                시드머니 : {" "}
+                <input
+                  id="money"
+                  type="number"
+                  value={money}
+                  onChange={moneyChange}
+                ></input> 
+              </label>
+            </div>
+          </form>
+          <hr />
+          <h2>{money}내가 살 수 있는 양은 {isNaN((money / itemPrice).toFixed(3)) ? null:(money / itemPrice).toFixed(3)}</h2>
+        </div>
+      )}
     </div>
   );
 }
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function App() {
+function ToDoList() {
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
   
@@ -26,4 +26,4 @@ function App() {
   );
 }
 
-export default App;
+export default ToDoList;
