ce6a44c3e14cb6ebfd665438aad049e12ef64bc6
dal10000 <dal10000@hanmail.net>
2023-07-04 09:49:36 +0900

    movie2

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  useEffect(()=>{
-    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year").then(res => res.json()).then(json => {
-      setMovies(json.data.movies);
-      setLoading(false);
-    })
-  },[]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   console.log(movies);
   return (
     <div>
-      {loading? <h3>loading....</h3> : null}
+      {loading ? (
+        <h3>loading....</h3>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <img src={movie.medium_cover_image} />
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
-// 
+// https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year
